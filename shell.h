02f4f46ee843647490b4947736d7e518d3be003e
« Ephely » <ephely1@gmail.com>
2025-01-08 15:26:25 +0100

    header file

--- shell.h
+++ shell.h
@@ -5,10 +5,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
+extern char **environ;
+
 #define BUFFER_SIZE 1024
 
+int exit_command(const char *input);
+char *read_input(void);
+void execute(char *input);
+int _strncmp(const char *s1, const char *s2, size_t n);
+char *_strtok(char *str, const char *tok);
+char *_strdup(char *str);
+int _strlen(char *s);
+unsigned int check_delim(char c, const char *str);
+char *find_command_path(char *command);
 
 #endif
