8de5c60d5a08fcc8aac8c380fd7a8aaffb02566b
Notsayy <jtertre25@gmail.com>
2025-01-08 17:23:09 +0100

    Delete the shell_loop file and just integrate it in our shell.c

---
+++ shell.c
@@ -0,0 +1,48 @@
+#include "shell.h"
+
+/**
+ * main - Entry point for the shell
+ * @argc: argc
+ * @argv: argv
+ * Return: EXIT_SUCCESS on success, EXIT_FAILURE on failure
+ */
+int main(__attribute__((unused)) int argc, char **argv)
+{
+	char *input = NULL;
+	size_t input_size = 0;
+	ssize_t input_count;
+	char *program_name = argv[0];
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		write(STDOUT_FILENO, "$ ", 2);
+
+		input_count = getline(&input, &input_size, stdin); /* Read the user input */
+
+		if (input_count == -1)
+		{
+			free(input);
+			break; /* Exit on EOF */
+		}
+
+		if (input_count > 0 && input[input_count - 1] == '\n')
+			input[input_count - 1] = '\0'; /* Remove newline */
+
+		if (strcmp(input, "exit") == 0) /* Handle 'exit' command */
+		{
+			free(input);
+			return (EXIT_SUCCESS);
+		}
+
+		char **args = split_line(input);
+
+		if (args[0] != NULL)
+		{
+			execute_command(args, program_name);
+		}
+
+		free(args);
+	}
+	return (EXIT_SUCCESS);
+}
