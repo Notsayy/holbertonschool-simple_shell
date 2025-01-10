6d9cfb6b5f2f51fd6f7ebd687ff1c7937732c541
Notsayy <jtertre25@gmail.com>
2025-01-10 01:32:16 +0100

    Add print environment

--- shell.c
+++ shell.c
@@ -73,6 +73,12 @@ static int process_input(char *input, char *program_name)
 	if (strcmp(input, "exit") == 0)
 		return (EXIT_FAILURE);
 
+	if (strcmp(input, "env") == 0)
+	{
+		print_environment();
+		return (EXIT_SUCCESS);
+	}
+
 	args = split_line(input);
 	if (args == NULL)
 		return (EXIT_SUCCESS);
@@ -83,7 +89,6 @@ static int process_input(char *input, char *program_name)
 	cleanup(NULL, args);
 	return ((status == 127) ? EXIT_SUCCESS : status);
 }
-
 /**
  * cleanup - Clean up resources
  * @input: The input string to free
@@ -95,4 +100,14 @@ static void cleanup(char *input, char **args)
 		free(input);
 	if (args != NULL)
 		free(args);
+}
+void print_environment(void)
+{
+	char **env = environ;
+
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
 }
